91bf67f291f45a2857c7a9503002b7034622e37c
jaanhavi9 <janhavi886@gmail.com>
2024-02-14 05:12:39 +0530

    fe

--- fe/main.py
+++ fe/main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/get_prices')
+def get_prices():
+    #process input
+    return render_template('price.html')
 if __name__ == '__main__':
     app.run(debug=True)
